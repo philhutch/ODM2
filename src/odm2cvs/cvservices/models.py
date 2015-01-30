82b66bcc3975c9da03df13eaf7e3bb1b563ccbab
Juan Caraballo <juan.caraballo17@gmail.com>
2015-01-30 16:11:33 -0700

    Simplified django models.

--- src/odm2cvs/cvservices/models.py
+++ src/odm2cvs/cvservices/models.py
@@ -3,219 +3,108 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class ActionType(models.Model):
-    term = models.TextField(primary_key=True)
-    name = models.TextField()
+class ControlVocabulary(models.Model):
+    term = models.CharField(primary_key=True, max_length=255)
+    name = models.CharField(max_length=255)
     definition = models.TextField()
-    category = models.TextField()
+    category = models.CharField(max_length=255)
     provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
+    provenanceuri = models.CharField(db_column='provenanceUri', blank=True, max_length=255)
     note = models.TextField(blank=True)
-    producesresult = models.TextField(db_column='producesResult')
+    
+    class Meta:
+        abstract = True
 
+
+class ControlVocabularyRequest(models.Model):
+    requestid = models.TextField(db_column='requestId', primary_key=True)
+    status = models.TextField()
+    datesubmitted = models.DateField(db_column='dateSubmitted')
+    datestatuschanged = models.DateField(db_column='dateStatusChanged')
+    requestnotes = models.TextField(db_column='requestNotes')
+    submittername = models.TextField(db_column='submitterName')
+    submitteremail = models.TextField(db_column='submitterEmail', blank=True)
+    requestreason = models.TextField(db_column='requestReason')
+
+    class Meta:
+        abstract = True
+
+
+class AbstractActionType(models.Model):
+    PRODUCES_RESULT_CHOICES = ('Yes', 'No')
+    producesresult = models.CharField(db_column='producesResult', max_length=5, choices=PRODUCES_RESULT_CHOICES)
+
+    class Meta:
+        abstract = True
+
+
+class ActionType(ControlVocabulary, AbstractActionType):
     class Meta:
         managed = False
         db_table = 'ActionTypeCV'
 
 
-class ActionTypeRequest(models.Model):
-    requestid = models.TextField(db_column='requestId', primary_key=True)
-    status = models.TextField()
-    term = models.ForeignKey(ActionType, db_column='term')
-    datesubmitted = models.DateField(db_column='dateSubmitted')
-    datestatuschanged = models.DateField(db_column='dateStatusChanged')
-    requestnotes = models.TextField(db_column='requestNotes')
-    submittername = models.TextField(db_column='submitterName')
-    submitteremail = models.TextField(db_column='submitterEmail', blank=True)
-    requestreason = models.TextField(db_column='requestReason')
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField()
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-    producesresult = models.TextField(db_column='producesResult')
-
+class ActionTypeRequest(ControlVocabularyRequest, ControlVocabulary, AbstractActionType):
     class Meta:
         managed = False
         db_table = 'ActionTypeCVRequests'
 
 
-class MethodType(models.Model):
-    term = models.TextField(primary_key=True)
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField()
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-
+class MethodType(ControlVocabulary):
     class Meta:
         managed = False
         db_table = 'MethodTypeCV'
 
 
-class MethodTypeRequest(models.Model):
-    requestid = models.TextField(db_column='requestId', primary_key=True)
-    status = models.TextField()
-    term = models.ForeignKey(MethodType, db_column='term')
-    datesubmitted = models.DateField(db_column='dateSubmitted')
-    datestatuschanged = models.DateField(db_column='dateStatusChanged')
-    requestnotes = models.TextField(db_column='requestNotes')
-    submittername = models.TextField(db_column='submitterName')
-    submitteremail = models.TextField(db_column='submitterEmail', blank=True)
-    requestreason = models.TextField(db_column='requestReason')
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField()
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-
+class MethodTypeRequest(ControlVocabularyRequest, ControlVocabulary):
     class Meta:
         managed = False
         db_table = 'MethodTypeCVRequests'
 
 
-class OrganizationType(models.Model):
-    term = models.TextField(primary_key=True)
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField(blank=True)
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-
+class OrganizationType(ControlVocabulary):
     class Meta:
         managed = False
         db_table = 'OrganizationTypeCV'
 
 
-class OrganizationTypeRequest(models.Model):
-    requestid = models.TextField(db_column='requestId', primary_key=True)
-    status = models.TextField()
-    term = models.ForeignKey(OrganizationType, db_column='term')
-    datesubmitted = models.DateField(db_column='dateSubmitted')
-    datestatuschanged = models.DateField(db_column='dateStatusChanged')
-    requestnotes = models.TextField(db_column='requestNotes')
-    submittername = models.TextField(db_column='submitterName')
-    submitteremail = models.TextField(db_column='submitterEmail', blank=True)
-    requestreason = models.TextField(db_column='requestReason')
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField(blank=True)
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-
+class OrganizationTypeRequest(ControlVocabularyRequest, ControlVocabulary):
     class Meta:
         managed = False
         db_table = 'OrganizationTypeCVRequests'
 
 
-class SamplingFeatureGeotype(models.Model):
-    term = models.TextField(primary_key=True)
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField()
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-
+class SamplingFeatureGeotype(ControlVocabulary):
     class Meta:
         managed = False
         db_table = 'SamplingFeatureGeotypeCV'
 
 
-class SamplingFeatureGeotypeRequest(models.Model):
-    requestid = models.TextField(db_column='requestId', primary_key=True)
-    status = models.TextField()
-    term = models.ForeignKey(SamplingFeatureGeotype, db_column='term')
-    datesubmitted = models.DateField(db_column='dateSubmitted')
-    datestatuschanged = models.DateField(db_column='dateStatusChanged')
-    requestnotes = models.TextField(db_column='requestNotes')
-    submittername = models.TextField(db_column='submitterName')
-    submitteremail = models.TextField(db_column='submitterEmail', blank=True)
-    requestreason = models.TextField(db_column='requestReason')
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField()
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-
+class SamplingFeatureGeotypeRequest(ControlVocabularyRequest, ControlVocabulary):
     class Meta:
         managed = False
         db_table = 'SamplingFeatureGeotypeCVRequests'
 
 
-class SamplingFeatureType(models.Model):
-    term = models.TextField(primary_key=True)
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField()
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-
+class SamplingFeatureType(ControlVocabulary):
     class Meta:
         managed = False
         db_table = 'SamplingFeatureTypeCV'
 
 
-class SamplingFeatureTypeRequest(models.Model):
-    requestid = models.TextField(db_column='requestId', primary_key=True)
-    status = models.TextField()
-    term = models.ForeignKey(SamplingFeatureType, db_column='term')
-    datesubmitted = models.DateField(db_column='dateSubmitted')
-    datestatuschanged = models.DateField(db_column='dateStatusChanged')
-    requestnotes = models.TextField(db_column='requestNotes')
-    submittername = models.TextField(db_column='submitterName')
-    submitteremail = models.TextField(db_column='submitterEmail', blank=True)
-    requestreason = models.TextField(db_column='requestReason')
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField()
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-
+class SamplingFeatureTypeRequest(ControlVocabularyRequest, ControlVocabulary):
     class Meta:
         managed = False
         db_table = 'SamplingFeatureTypeCVRequests'
 
 
-class SiteType(models.Model):
-    term = models.TextField(primary_key=True)
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField()
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-
+class SiteType(ControlVocabulary):
     class Meta:
         managed = False
         db_table = 'SiteTypeCV'
 
 
-class SiteTypeRequest(models.Model):
-    requestid = models.TextField(db_column='requestId', primary_key=True)
-    status = models.TextField()
-    term = models.ForeignKey(SiteType, db_column='term')
-    datesubmitted = models.DateField(db_column='dateSubmitted')
-    datestatuschanged = models.DateField(db_column='dateStatusChanged')
-    requestnotes = models.TextField(db_column='requestNotes')
-    submittername = models.TextField(db_column='submitterName')
-    submitteremail = models.TextField(db_column='submitterEmail', blank=True)
-    requestreason = models.TextField(db_column='requestReason')
-    name = models.TextField()
-    definition = models.TextField()
-    category = models.TextField()
-    provenance = models.TextField(blank=True)
-    provenanceuri = models.TextField(db_column='provenanceUri', blank=True)
-    note = models.TextField(blank=True)
-
+class SiteTypeRequest(ControlVocabularyRequest, ControlVocabulary):
     class Meta:
         managed = False
         db_table = 'SiteTypeCVRequests'
