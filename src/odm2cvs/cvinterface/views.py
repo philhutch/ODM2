b2c01e4c95c9844fddb31429b960aa955845feeb
Juan Caraballo <juan.caraballo17@gmail.com>
2015-02-04 20:57:34 -0700

    Changed the mechanism for views loading.

--- src/odm2cvs/cvinterface/views.py
+++ src/odm2cvs/cvinterface/views.py
@@ -41,131 +41,83 @@ class HomeView(ListView):
 class ActionTypeView(VocabularyListView):
     model = ActionType
     queryset = ActionType.objects.using('control_vocabularies')
-    template_name = 'cvinterface/list.html'
-    detail_view = None
-
-    def get_context_data(self, **kwargs):
-        context = super(ActionTypeView, self).get_context_data(**kwargs)
-        urls = [reverse(self.detail_view, kwargs={'pk': cv.term}) for cv in context['object_list']]
-        context['vocabulary_objects'] = zip(context['object_list'], urls)
-        return context
+    template_name = 'cvinterface/vocabularies/actiontype_list.html'
 
 
 class MethodTypeView(VocabularyListView):
     model = MethodType
     queryset = MethodType.objects.using('control_vocabularies')
-    template_name = 'cvinterface/list.html'
-    detail_view = None
-
-    def get_context_data(self, **kwargs):
-        context = super(MethodTypeView, self).get_context_data(**kwargs)
-        urls = [reverse(self.detail_view, kwargs={'pk': cv.term}) for cv in context['object_list']]
-        context['vocabulary_objects'] = zip(context['object_list'], urls)
-        return context
+    template_name = 'cvinterface/vocabularies/methodtype_list.html'
 
 
 class OrganizationTypeView(VocabularyListView):
     model = OrganizationType
     queryset = OrganizationType.objects.using('control_vocabularies')
-    template_name = 'cvinterface/list.html'
-    detail_view = None
-
-    def get_context_data(self, **kwargs):
-        context = super(OrganizationTypeView, self).get_context_data(**kwargs)
-        urls = [reverse(self.detail_view, kwargs={'pk': cv.term}) for cv in context['object_list']]
-        context['vocabulary_objects'] = zip(context['object_list'], urls)
-        return context
+    template_name = 'cvinterface/vocabularies/organizationtype_list.html'
 
 
 class SamplingFeatureGeotypeView(VocabularyListView):
     model = SamplingFeatureGeotype
     queryset = SamplingFeatureGeotype.objects.using('control_vocabularies')
-    template_name = 'cvinterface/list.html'
-    detail_view = None
-
-    def get_context_data(self, **kwargs):
-        context = super(SamplingFeatureGeotypeView, self).get_context_data(**kwargs)
-        urls = [reverse(self.detail_view, kwargs={'pk': cv.term}) for cv in context['object_list']]
-        context['vocabulary_objects'] = zip(context['object_list'], urls)
-        return context
+    template_name = 'cvinterface/vocabularies/samplingfeaturegeotype_list.html'
 
 
 class SamplingFeatureTypeView(VocabularyListView):
     model = SamplingFeatureType
     queryset = SamplingFeatureType.objects.using('control_vocabularies')
-    template_name = 'cvinterface/list.html'
-    detail_view = None
-
-    def get_context_data(self, **kwargs):
-        context = super(SamplingFeatureTypeView, self).get_context_data(**kwargs)
-        urls = [reverse(self.detail_view, kwargs={'pk': cv.term}) for cv in context['object_list']]
-        context['vocabulary_objects'] = zip(context['object_list'], urls)
-        return context
+    template_name = 'cvinterface/vocabularies/samplingfeaturetype_list.html'
 
 
 class SiteTypeView(VocabularyListView):
     model = SiteType
     queryset = SiteType.objects.using('control_vocabularies')
-    template_name = 'cvinterface/list.html'
-    detail_view = None
-
-    def get_context_data(self, **kwargs):
-        context = super(SiteTypeView, self).get_context_data(**kwargs)
-        urls = [reverse(self.detail_view, kwargs={'pk': cv.term}) for cv in context['object_list']]
-        context['vocabulary_objects'] = zip(context['object_list'], urls)
-        return context
+    template_name = 'cvinterface/vocabularies/sitetype_list.html'
 
 
 # Detail Views
 class ActionTypeDetailView(DetailView):
     model = ActionType
     queryset = ActionType.objects.using('control_vocabularies')
-    template_name = 'cvinterface/term.html'
+    template_name = 'cvinterface/vocabularies/actiontype_detail.html'
 
 
 class MethodTypeDetailView(DetailView):
     model = MethodType
     queryset = MethodType.objects.using('control_vocabularies')
-    template_name = 'cvinterface/term.html'
+    template_name = 'cvinterface/vocabularies/methodtype_detail.html'
 
 
 class OrganizationTypeDetailView(DetailView):
     model = OrganizationType
     queryset = OrganizationType.objects.using('control_vocabularies')
-    template_name = 'cvinterface/term.html'
+    template_name = 'cvinterface/vocabularies/organizationtype_detail.html'
 
 
 class SamplingFeatureGeotypeDetailView(DetailView):
     model = SamplingFeatureGeotype
     queryset = SamplingFeatureGeotype.objects.using('control_vocabularies')
-    template_name = 'cvinterface/term.html'
+    template_name = 'cvinterface/vocabularies/samplingfeaturegeotype_detail.html'
 
 
 class SamplingFeatureTypeDetailView(DetailView):
     model = SamplingFeatureType
     queryset = SamplingFeatureType.objects.using('control_vocabularies')
-    template_name = 'cvinterface/term.html'
+    template_name = 'cvinterface/vocabularies/samplingfeaturetype_detail.html'
 
 
 class SiteTypeDetailView(DetailView):
     model = SiteType
     queryset = SiteType.objects.using('control_vocabularies')
-    template_name = 'cvinterface/term.html'
+    template_name = 'cvinterface/vocabularies/sitetype_detail.html'
 
 
-action_type_detail_view = ActionTypeDetailView.as_view()
-method_type_detail_view = MethodTypeDetailView.as_view()
-organization_type_detail_view = OrganizationTypeDetailView.as_view()
-sampling_feature_geotype_detail_view = SamplingFeatureGeotypeDetailView.as_view()
-sampling_feature_type_detail_view = SamplingFeatureTypeDetailView.as_view()
-site_type_detail_view = SiteTypeDetailView.as_view()
+action_type_view = ActionTypeView.as_view()
+method_type_view = MethodTypeView.as_view()
+organization_type_view = OrganizationTypeView.as_view()
+sampling_feature_geotype_view = SamplingFeatureGeotypeView.as_view()
+sampling_feature_type_view = SamplingFeatureTypeView.as_view()
+site_type_view = SiteTypeView.as_view()
 
-action_type_view = ActionTypeView.as_view(detail_view=action_type_detail_view)
-method_type_view = MethodTypeView.as_view(detail_view=method_type_detail_view)
-organization_type_view = OrganizationTypeView.as_view(detail_view=organization_type_detail_view)
-sampling_feature_geotype_view = SamplingFeatureGeotypeView.as_view(detail_view=sampling_feature_geotype_detail_view)
-sampling_feature_type_view = SamplingFeatureTypeView.as_view(detail_view=sampling_feature_type_detail_view)
-site_type_view = SiteTypeView.as_view(detail_view=site_type_detail_view)
 
 view_functions = {ActionTypeView: action_type_view, MethodTypeView: method_type_view,
                   OrganizationTypeView: organization_type_view,
--- src/odm2cvs/odm2cvs/urls.py
+++ src/odm2cvs/odm2cvs/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
 
-from cvinterface.views import HomeView, action_type_view, method_type_view, organization_type_view, \
-    sampling_feature_geotype_view, sampling_feature_type_view, site_type_view, \
-    action_type_detail_view, method_type_detail_view, organization_type_detail_view, \
-    sampling_feature_geotype_detail_view, sampling_feature_type_detail_view, site_type_detail_view
+from cvinterface.views import HomeView, ActionTypeDetailView, MethodTypeDetailView, OrganizationTypeDetailView, \
+    SamplingFeatureGeotypeDetailView, SamplingFeatureTypeDetailView, SiteTypeDetailView
+
+from cvinterface.views import action_type_view, method_type_view, organization_type_view, \
+    sampling_feature_geotype_view, sampling_feature_type_view, site_type_view
 
 from cvservices.api import ActionTypeResource, ActionTypeRequestResource, MethodTypeResource, \
     MethodTypeRequestResource, OrganizationTypeResource, OrganizationTypeRequestResource, \
@@ -30,18 +31,22 @@ urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^api/', include(v1_api.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^actiontype/$', action_type_view, name='actiontypecv'),
-    url(r'^methodtype/$', method_type_view, name='methodtypecv'),
-    url(r'^organizationtype/$', organization_type_view, name='organizationtypecv'),
-    url(r'^samplingfeaturegeotype/$', sampling_feature_geotype_view, name='samplingfeaturegeotypecv'),
-    url(r'^samplingfeaturetype/$', sampling_feature_type_view, name='samplingfeaturetypecv'),
-    url(r'^sitetype/$', site_type_view, name='sitetypecv'),
 
-    url(r'^actiontype/(?P<pk>\w+)/$', action_type_detail_view, name='actiontypecv_detail'),
-    url(r'^methodtype/(?P<pk>\w+)/$', method_type_detail_view, name='methodtypecv_detail'),
-    url(r'^organizationtype/(?P<pk>\w+)/$', organization_type_detail_view, name='organizationtypecv_detail'),
-    url(r'^samplingfeaturegeotype/(?P<pk>\w+)/$', sampling_feature_geotype_detail_view, name='samplingfeaturegeotypecv_detail'),
-    url(r'^samplingfeaturetype/(?P<pk>\w+)/$', sampling_feature_type_detail_view, name='samplingfeaturetypecv_detail'),
-    url(r'^sitetype/(?P<pk>\w+)/$', site_type_detail_view, name='sitetypecv_detail'),
+    url(r'^actiontype/$', action_type_view, name='actiontype'),
+    url(r'^actiontype/(?P<pk>\w+)/$', ActionTypeDetailView.as_view(), name='actiontype_detail'),
 
+    url(r'^methodtype/$', method_type_view, name='methodtype'),
+    url(r'^methodtype/(?P<pk>\w+)/$', MethodTypeDetailView.as_view(), name='methodtype_detail'),
+
+    url(r'^organizationtype/$', organization_type_view, name='organizationtype'),
+    url(r'^organizationtype/(?P<pk>\w+)/$', OrganizationTypeDetailView.as_view(), name='organizationtype_detail'),
+
+    url(r'^samplingfeaturegeotype/$', sampling_feature_geotype_view, name='samplingfeaturegeotype'),
+    url(r'^samplingfeaturegeotype/(?P<pk>\w+)/$', SamplingFeatureGeotypeDetailView.as_view(), name='samplingfeaturegeotype_detail'),
+
+    url(r'^samplingfeaturetype/$', sampling_feature_type_view, name='samplingfeaturetype'),
+    url(r'^samplingfeaturetype/(?P<pk>\w+)/$', SamplingFeatureTypeDetailView.as_view(), name='samplingfeaturetype_detail'),
+
+    url(r'^sitetype/$', site_type_view, name='sitetype'),
+    url(r'^sitetype/(?P<pk>\w+)/$', SiteTypeDetailView.as_view(), name='sitetype_detail'),
 )
