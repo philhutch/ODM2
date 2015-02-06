eddcf3d450857258dfc0edd4ac1cd50330176054
Denver Smith <denverpsmith@gmail.com>
2015-02-06 16:34:00 -0700

    Updated API

--- src/odm2cvs/rdfserializer/api.py
+++ src/odm2cvs/rdfserializer/api.py
@@ -45,16 +45,15 @@ class RdfSerializer(Serializer):
         graph = Graph()
         skos = ns('http://www.w3.org/2004/02/skos/core#')
         odm2 = ns(baseURI)
+        #odm2 = ns('/home/denver/Documents/WaterLab/ODM2/src/odm2cvs/rdfserializer')
         dc = ns('http://purl.org/dc/elements/1.1/')
 
         graph.bind('odm2', odm2)
         graph.bind('skos', skos)
         graph.bind('dc', dc)
 
-
         if isinstance(data, dict):
             if u'objects' in data:
-                print type(scheme.title), scheme.title
                 graph.add((URIRef(scheme.uri), RDF['type'], skos['ConceptScheme']))
                 graph.add((URIRef(scheme.uri), dc['title'], Literal(scheme.title)))
                 graph.add((URIRef(scheme.uri), dc['creator'], Literal(scheme.creator)))
@@ -65,14 +64,18 @@ class RdfSerializer(Serializer):
                     graph.add((URIRef(scheme.uri + '/' + concept.obj.term), skos['inScheme'], URIRef(scheme.uri)))
                     #graph.add((URIRef(scheme.uri + '/' + concept.data[u'term']), RDF['type'], skos['Concept']))
                     for x in concept.data:
-                        if x != u'resource_uri' and x != 'term':
+                        if x == u'resource_uri' or x == 'term':
+                            continue
+                        elif concept.data[x] == '':
+                            continue
+                        #if x != u'resource_uri' and x != 'term':
+                        else:
                             alias = str(FieldRelation.objects.get(field_name=x).node.namespace)
                             if alias == 'odm2':
                                 graph.add((URIRef(scheme.uri + '/' + concept.obj.term), odm2[FieldRelation.objects.get(field_name=x).node.name], Literal(concept.data[x])))
                             else:
                                 graph.add((URIRef(scheme.uri + '/' + concept.obj.term), skos[FieldRelation.objects.get(field_name=x).node.name], Literal(concept.data[x])))
             else:
-                # weird...
                 pass
         elif isinstance(data, Bundle):
             graph.add((URIRef(scheme.uri + '/' + data.obj.term), RDF['type'], skos['Concept']))
@@ -80,16 +83,18 @@ class RdfSerializer(Serializer):
             for field in data.data.keys():
                 if field == 'term' or field == u'resource_uri':
                     continue
+                elif data.data[field] == '':
+                    continue
                 else:
                     relation = FieldRelation.objects.get(field_name=field)
                     alias = relation.node.namespace.alias
                     if alias == u'odm2':
-                        graph.add((URIRef(scheme.uri + '/' + data.obj.term), odm2[FieldRelation.objects.get(field_name=field).node.name], Literal(data.data[field])))
+                        graph.add((URIRef(scheme.uri + '/' + data.obj.term), odm2[FieldRelation.objects.get(field_name=field).node.name], Literal(str(data.data[field].rstrip('\r\n')))))
                     else:
                         graph.add((URIRef(scheme.uri + '/' + data.obj.term), skos[FieldRelation.objects.get(field_name=field).node.name], Literal(data.data[field])))
             
         else:
-            # uhm...
+            print type(data)
             pass
         return graph.serialize(format='pretty-xml')
-        #return data[u'objects']
+
