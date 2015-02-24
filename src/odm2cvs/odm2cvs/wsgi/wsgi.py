958330b7b3ebe627e85124c7cdec7f56308cc9e1
Denver <denverpsmith@gmail.com>
2015-02-24 15:46:09 -0700

    Added wsgi configuration for the apache sandbox server.

--- src/odm2cvs/odm2cvs/wsgi/wsgi.py
+++ src/odm2cvs/odm2cvs/wsgi/wsgi.py
@@ -1,4 +1,5 @@
 """
+
 WSGI config for odm2cvs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -8,6 +9,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+print os.environ
+sys.path.append(os.environ["ODM2CVS_PROJECT_ROOT"])
+#sys.path.append('/home/denver/projects/ODM2/src/odm2cvs')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "odm2cvs.settings")
 
 from django.core.wsgi import get_wsgi_application
